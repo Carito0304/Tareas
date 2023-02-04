7d82ad1b9779d16082ec2273f2ca54ff1ea2753f
Carito Morera <105758588+Carito0304@users.noreply.github.com>
2023-02-04 02:34:39 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,44 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tarea_1
-{//Profe, con mucho amor y dedicación me quedé hasta las 12:17 am haciendo esto JAJAJAJ
+namespace Tarea_2
+{
 	internal class Program
 	{
+		
+        static string[] nombre = new string[3]; 		
+		static float[] notas = new float[3]; 
+		static byte indice = 0; 
 		static void Main(string[] args)
 		{
+			Estudiante.menu();
+			Console.Read();
 
-			int[] edad = new int[] { 20, 22, 33 };
-			float[] Salarios = new float[] { 200, 300, 150 };
-			String[] musico = new string[3];
-			musico[0] = "Ringo";
-			musico[1] = "Paul";
-			musico[2] = "John";
-			float total = 0;
-			for (int i = 0; i < musico.Length; i++)
-			{
-				// double IVA = Salarios[i] * 0.13;
-				float IVA = 0;
-				IVA = Salarios[i] * 0.13f;
-				Console.WriteLine($"{musico}[i] EDAD: {edad[i]} Salario: {Salarios[i]} IVA:{IVA} Total:{Salarios[i]+IVA} ");
-				
-				
 			
-			}
-			Console.ReadLine();
-
-
-
-
-
-
-
-
-
-
-
-
+		
+		
 		}
 	}
-
 }
